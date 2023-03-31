f70103bcd9837a9aeb24c9e033efa566c9aac0c7
ethyi <ethyidev@gmail.com>
2023-03-31 11:36:44 -0700

    log for handling account

--- nft_ingester/src/program_transformers/mod.rs
+++ nft_ingester/src/program_transformers/mod.rs
@@ -144,6 +144,7 @@ impl ProgramTransformer {
         &self,
         acct: AccountInfo<'b>,
     ) -> Result<(), IngesterError> {
+        info!("Handling Account: {:?}", acct.pubkey());
         let owner = acct.owner().unwrap();
         if let Some(program) = self.match_program(owner) {
             let result = program.handle_account(&acct)?;
