1636f185e0fe9cf34caf2c57fcdf1e971543b679
Nicolas Pennie <nicolas.pennie@gmail.com>
2023-05-15 19:07:48 -0700

    fix(ingester): fix compressed nft burn indexing

--- nft_ingester/src/program_transformers/bubblegum/burn.rs
+++ nft_ingester/src/program_transformers/bubblegum/burn.rs
@@ -1,12 +1,9 @@
-use crate::{
-    error::IngesterError,
-};
-use super::{update_asset, save_changelog_event};
-use blockbuster::{
-    instruction::InstructionBundle,
-    programs::bubblegum::{BubblegumInstruction, LeafSchema},
-};
+use super::{save_changelog_event, update_asset};
+use crate::error::IngesterError;
+use anchor_lang::prelude::Pubkey;
+use blockbuster::{instruction::InstructionBundle, programs::bubblegum::BubblegumInstruction};
 use digital_asset_types::dao::asset;
+use log::debug;
 use sea_orm::{entity::*, ConnectionTrait, TransactionTrait};
 
 pub async fn burn<'c, T>(
@@ -17,23 +14,29 @@ pub async fn burn<'c, T>(
 where
     T: ConnectionTrait + TransactionTrait,
 {
-    if let (Some(le), Some(cl)) = (&parsing_result.leaf_update, &parsing_result.tree_update) {
+    if let Some(cl) = &parsing_result.tree_update {
         let seq = save_changelog_event(cl, bundle.slot, txn).await?;
-        return match le.schema {
-            LeafSchema::V1 { id, .. } => {
-                let id_bytes = id.to_bytes().to_vec();
-                let asset_to_update = asset::ActiveModel {
-                    id: Unchanged(id_bytes.clone()),
-                    burnt: Set(true),
-                    seq: Set(seq as i64), // gummyroll seq
-                    ..Default::default()
-                };
-                // Don't send sequence number with this update, because we will always
-                // run this update even if it's from a backfill/replay.
-                update_asset(txn, id_bytes, None, asset_to_update).await
-            }
-            _ => Err(IngesterError::NotImplemented),
+        let leaf_index = cl.index;
+        let (asset_id, _) = Pubkey::find_program_address(
+            &[
+                "asset".as_bytes(),
+                cl.id.as_ref(),
+                leaf_index.to_le_bytes().as_ref(),
+            ],
+            &mpl_bubblegum::ID,
+        );
+        debug!("Indexing burn for asset id: {:?}", asset_id);
+        let id_bytes = asset_id.to_bytes().to_vec();
+        let asset_to_update = asset::ActiveModel {
+            id: Unchanged(id_bytes.clone()),
+            burnt: Set(true),
+            seq: Set(seq as i64),
+            ..Default::default()
         };
+        // Don't send sequence number with this update, because we will always
+        // run this update even if it's from a backfill/replay.
+        update_asset(txn, id_bytes, None, asset_to_update).await?;
+        return Ok(());
     }
     Err(IngesterError::ParsingError(
         "Ix not parsed correctly".to_string(),
